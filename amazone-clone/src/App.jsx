001fac1aa7ce2447bec742dd9eb80997d2847d37
girum32 <girum32@gmail.com>
2025-02-28 21:29:30 -0600

    Carousel section created

--- amazone-clone/src/App.jsx
+++ amazone-clone/src/App.jsx
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 
 import "./App.css";
 import Header from "./components/Header/Header";
+import { Carousel } from "react-responsive-carousel";
+import CarouselEffect from "./components/Carousel/Carousel";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,6 +11,7 @@ function App() {
   return (
     <>
       <Header/>
+      <CarouselEffect/>
     </> 
   );
 }
